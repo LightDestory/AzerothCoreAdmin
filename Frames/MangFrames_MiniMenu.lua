d2abdc7f083039af83245635a1b80afeabfec8e7
superstyro <superstyro@gmail.com>
2018-10-18 23:34:05 -0700

    Resize Minimenu for removed buttons

--- Frames/MangFrames_MiniMenu.lua
+++ Frames/MangFrames_MiniMenu.lua
@@ -45,7 +45,7 @@ function MangAdmin:CreateMiniMenu()
     draggable = true,
     size = {
       width = 28,
-      height = 377
+      height = 244
     },
     setpoint = {
       pos = "RIGHT"
@@ -62,7 +62,7 @@ function MangAdmin:CreateMiniMenu()
     },
     size = {
       width = 24,
-      height = 372
+      height = 240
     },
     setpoint = {
       pos = "TOPLEFT",
