07b2b0bd3f24ec59d8dbdf0bb81ebaa900ea3faf
Tylerlee Burhans <superstyro@gmail.com>
2019-10-18 18:19:59 -0700

    Update Version 057, add DiffTime

--- TrinityAdmin.lua
+++ TrinityAdmin.lua
@@ -34,7 +34,7 @@ gettingGOBinfo=0
 gettingGOBinfoinfo=0
 
 MAJOR_VERSION = "TrinityAdmin-3.3.5"
-MINOR_VERSION = "$Revision: 055 $"
+MINOR_VERSION = "$ Revision: 057 $"
 ROOT_PATH     = "Interface\\AddOns\\TrinityAdmin\\"
 local cont = ""
 if not AceLibrary then error(MAJOR_VERSION .. " requires AceLibrary") end
@@ -867,11 +867,16 @@ function MangAdmin:AddMessage(frame, text, r, g, b, id)
         end
       end
     end
-    for diff in string.gmatch(text, Strings["ma_GmatchUpdateDiff"]) do
-        ma_difftext:SetText(diff)
-        catchedSth = true
---        output = MangAdmin.db.account.style.showchat
-        output = MangAdmin.db.account.style.showchat
+--     for diff in string.gmatch(text, Strings["ma_GmatchUpdateDiff"]) do
+--         ma_difftext:SetText(diff)
+--         catchedSth = true
+-- --        output = MangAdmin.db.account.style.showchat
+--         output = MangAdmin.db.account.style.showchat
+--     end
+    for diff in string.gmatch(text, Strings[ma_GmatchUpdateDiffTime]) do --Only want the Diff time value
+      ma_difftext:SetText(diff)
+      catchedSth = true
+      output = MangAdmin.db.account.style.showchat
     end
     -- hook all new tickets
     for name in string.gmatch(text, Strings["ma_GmatchNewTicket"]) do
